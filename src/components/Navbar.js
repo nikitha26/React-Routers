e6edf2f09e0ad78fb18f6da8ce4c83c71d3c7aac
nikitha26 <mnikitha365@gmail.com>
2021-05-18 20:10:34 +0530

    create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+function Navbar() {
+    return (
+    <>
+      <nav className="navbar">
+          <div className="navbar-container">
+              <Link to="/" className="navbar-logo">
+                   TLVL
+              </Link>
+          </div>
+      </nav>
+    </>
+    )
+}
+
+export default Navbar
