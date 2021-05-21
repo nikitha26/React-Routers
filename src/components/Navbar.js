20b86ce9011b82674b44772e3d95a0ecfb4977cc
nikitha26 <mnikitha365@gmail.com>
2021-05-21 19:52:24 +0530

    font awesome added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css'
 function Navbar() {
     return (
     <>
       <nav className="navbar">
           <div className="navbar-container">
               <Link to="/" className="navbar-logo">
-                   TLVL
+                   TLVL <i class="fa fa-diamond" aria-hidden="true"></i>
+              </Link>
+              <Link to="" className="">
+                    HOME
+              </Link>
+              <Link to="" className="">
+                    ABOUT
               </Link>
           </div>
       </nav>
