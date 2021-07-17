6758caf5bbc3620bf300708d7dd955509499934e
nikitha26 <mnikitha365@gmail.com>
2021-07-17 20:22:15 +0530

    add container

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './Signup.css';
-import Button from '@material-ui/core/Button';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 function Signup(){
      return (
-         <div className="form_div">
-             <h2>SignUp</h2>
-             
-                  <form>
-                    <Button variant="contained" color="primary">
-                            SIGNUP
-                    </Button>
-                  </form>
-         </div>    
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <Typography component="div" style={{ backgroundColor: '#cfe8fc',height:100,}} >
+                <h1>SIGN UP</h1>
+                 </Typography>  
+            </Container>
+      </React.Fragment>
      )
 }
 export default Signup;
