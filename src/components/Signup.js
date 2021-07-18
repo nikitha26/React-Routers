858c1795db4392b685e66ad955cd6e8a22d44b9c
nikitha26 <mnikitha365@gmail.com>
2021-07-18 19:55:36 +0530

    signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,51 @@
 import React from "react";
 import './Signup.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+     root: {
+       '& > *': {
+         margin: theme.spacing(1),
+         width: '50ch',
+         margin:25,
+        
+       },
+     },
+   }));
+
 function Signup(){
+          const classes = useStyles();
+
      return (
         <React.Fragment>
             <CssBaseline />
-            <Container maxWidth="sm">
-                <Typography component="div" style={{ backgroundColor: '#cfe8fc',height:100,}} >
-                <h1>SIGN UP</h1>
-                 </Typography>  
-            </Container>
+               <Container maxWidth="sm" className="form_block">
+                   <h1 className="signup_heading">SIGN UP</h1>
+                   <h3 >Already a member ? <a href="" className="Log_in"> Log in</a></h3>
+                    <form className={classes.root} noValidate autoComplete="off" 
+                       style={{marginLeft:"60px"}} 
+                    >
+                         <TextField 
+                              id="standard-basic" 
+                              label="Enter Email" 
+                              
+                         />
+                          {/* <i class="fa fa-envelope" aria-hidden="true"></i> */}
+                         <TextField
+                              id="standard-password-input"
+                              label="Enter Password"
+                              type="password"
+                              autoComplete="current-password"
+                         />
+                         <Button variant="contained" color="primary" className="signup_btn">
+                             SIGN UP
+                         </Button>
+                    </form>
+               </Container>
       </React.Fragment>
      )
 }
