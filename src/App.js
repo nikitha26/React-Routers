484b8c0e51592cb29fa52d4bc0845d4f3db3aae3
nikitha26 <mnikitha365@gmail.com>
2021-12-12 22:50:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ function App() {
   return (
     <Router>
       <Navbar/>
+      <Route exact path = "/home" component={App}> </Route>
+
       <Switch>
-        <Route path="/" exact component={Home}/>    
         <Route path="/services" exact component={Services}/>   
         <Route path="/about" exact component={About}/>    
         <Route path="/signup" exact component={SignUp}/>
